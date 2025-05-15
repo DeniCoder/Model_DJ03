0767cbc26833cdc151abec3a504f93af386be4c3
DeniCoder <denis2042@yandex.ru>
2025-05-15 13:19:29 +0300

    add last change
    news post

--- Model_DJ03/news/urls.py
+++ Model_DJ03/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home_news')
+    path('', views.news, name='news'),
+    path('news/<int:post_id>/', views.news_detail, name='news_detail'),
 ]
--- Model_DJ03/news/views.py
+++ Model_DJ03/news/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News_post
 
 # Create your views here.
-def home(request):
-    news = News_post.objects.all()
-    return render(request, 'news/news.html', {'news' : news})
+def news(request):
+    posts = News_post.objects.all()
+    return render(request, 'news/news.html', {'posts' : posts})
 
+def news_detail(request, post_id):
+    post = get_object_or_404(News_post, id=post_id)
+    return render(request, 'news/news_detail.html', {'post': post})
